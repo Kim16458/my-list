f25e8738f33687567b1877b2c0d24f1d4cb2c36f
Kim16458 <kirimiochola@gmail.com>
2024-03-03 16:01:37 +0300

    Create list.py

---
+++ list.py
@@ -0,0 +1,26 @@
+
+my_list = [10, 20, 30, 40]
+print(my_list)
+
+my_list.insert(1, 15)
+print(my_list)
+
+my_list2 = [50, 60, 70]
+my_list.extend(my_list2)
+print(my_list)
+
+my_list.pop(7)
+print(my_list)
+
+my_list.sort(reverse=True)
+print(my_list)
+my_list.sort()
+print(my_list)
+
+position = 3
+print(my_list[position])
+
+
+
+
+
